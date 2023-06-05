055f36829bd455824fbcca35ed3a86b40c8b9803
Remy Zulauff <remy.zulauff@gmail.com>
2023-06-05 21:26:43 +0200

    refactor(client.py): move import statements to the top and add import for transport module feat(client.py): add support for http_transport parameter to be able to use different http transports feat(client.py): add get_transport_from_settings method to get transport from settings and use it in constructor

--- odooaiorpc/client.py
+++ odooaiorpc/client.py
@@ -1,26 +1,42 @@
 import typing as t
 
-from config import OdooSettings
 from pydantic import AnyUrl
+
+from odooaiorpc import const, transport
+from odooaiorpc.config import OdooSettings
 
 
 class OdooAioRPC:
     def __init__(
-        self, url: t.Union[str, AnyUrl], database: str, user: str, secret: str
+        self,
+        url: t.Union[str, AnyUrl],
+        database: str,
+        user: str,
+        secret: str,
+        http_transport: transport.BaseTransport,
     ) -> None:
         """
-        _summary_
+        Initialize OdooAioRPC client
+        A transport must be provided
 
         Args:
             url (t.Union[str, AnyUrl]): Odoo URL
-            database (str): Database name
-            user (str): User used to communicate
-            secret (str): API key or password
+            database (str): Odoo database
+            user (str): Odoo user to connect with
+            secret (str): Odoo user generated api key or password (discouraged)
+            http_transport (transport.BaseTransport): Http Transport
         """
         self._url = url
         self._database = database
         self._user = user
         self._secret = secret
+        self._transport = http_transport
+
+    @staticmethod
+    def get_transport_from_settings(settings: OdooSettings) -> transport.BaseTransport:
+        if settings.protocol == const.Protocol.xmlrpc:
+            return transport.XmlRpcProtocol.from_settings(settings)
+        return transport.JsonRpcProtocol.from_settings(settings)
 
     @classmethod
     def from_settings(cls, settings: OdooSettings) -> "OdooAioRPC":
@@ -29,4 +45,5 @@ class OdooAioRPC:
             database=t.cast(str, settings.database),
             user=settings.user,
             secret=settings.secret,
+            http_transport=cls.get_transport_from_settings(settings),
         )
